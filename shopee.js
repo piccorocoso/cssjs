830245b38e0b22346cfcc4d644a5f609a5de7a51
piccorocoso <35660307+piccorocoso@users.noreply.github.com>
2024-06-17 15:03:14 +0700

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -1,6 +1,6 @@
-let t=' (th)'
-q={'39925189':'BBS_Shop'+t,'87124376':'Technic'+t,'57748591':'PADPAD'+t,'75734101':'Plus-elec'+t,'594259220':'NT_SOUND'+t,'232977921':'Rungrueng'+t,'98860810':'S.G '+t,'142008414':'skybox'+t,'59899598':'ICE'+t,'82250234':'SV'+t,'49672794':'Micro.C'+t,
-'92699435':'ดำรง'+t,'996599594':'บ้านหม้อ'+t,
+const ths=' (th)';
+let q={'39925189':'BBS_Shop'+ths,'87124376':'Technic'+ths,'57748591':'PADPAD'+ths,'75734101':'Plus-elec'+ths,'594259220':'NT_SOUND'+ths,'232977921':'Rungrueng'+ths,'98860810':'S.G '+ths,'142008414':'skybox'+ths,'59899598':'ICE'+ths,'82250234':'SV'+ths,'49672794':'Micro.C'+ths,
+'92699435':'ดำรง'+ths,'996599594':'บ้านหม้อ'+ths,
 /*
 '':'',
 '':'',
@@ -11,4 +11,4 @@ q={'39925189':'BBS_Shop'+t,'87124376':'Technic'+t,'57748591':'PADPAD'+t,'7573410
 */
 '780789080':'EC.my','998571379':'GOHJMY','824106152':'HJXRHGAL','889717687':'Intergar.th','196639923':'Uyghur','331414085':'inventor','140994668':'ffcc','288377805':'MuseAudio1','1049437441':'Sintovi','392696149':'CAZENOVEYI','783819622':'ICeeStore','753519052':'Yanlong','59553004':'relandor.th','342477205':'sydz568.th','91634568':'MCIGICM','442455922':'GouZA','497930211':'kaynes','1093213423':'caiwenwei','1020555463':'allguitar','734473806':'he_shuo','140735017':'shiwaki1','445652898':'Electronicworld','830034949':'BXV_components','240783242':'reland2','373010132':'lkshop',
 }
-alert(q['998571379'])
+alert(q['39925189'])
