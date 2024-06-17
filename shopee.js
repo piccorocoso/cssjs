93f2765de9391eb422bde947623c08ad8e30a2d9
piccorocoso <35660307+piccorocoso@users.noreply.github.com>
2024-06-17 22:48:02 +0700

    Update shopee.js

--- shopee.js
+++ shopee.js
@@ -12,3 +12,4 @@ let q={'39925189':'BBS_Shop'+ths,'87124376':'Technic'+ths,'57748591':'PADPAD'+th
 '780789080':'EC.my','998571379':'GOHJMY','824106152':'HJXRHGAL','889717687':'Intergar.th','196639923':'Uyghur','331414085':'inventor','140994668':'ffcc','288377805':'MuseAudio1','1049437441':'Sintovi','392696149':'CAZENOVEYI','783819622':'ICeeStore','753519052':'Yanlong','59553004':'relandor.th','342477205':'sydz568.th','91634568':'MCIGICM','442455922':'GouZA','497930211':'kaynes','1093213423':'caiwenwei','1020555463':'allguitar','734473806':'he_shuo','140735017':'shiwaki1','445652898':'Electronicworld','830034949':'BXV_components','240783242':'reland2','373010132':'lkshop',
 }
 //alert(q['780789080'])
+console.log(q['780789080'])
